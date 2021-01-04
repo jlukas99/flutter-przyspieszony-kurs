c9c862e8dad85af859c8a764c6093118f574f9e4
Lukas <lukasjanek99@gmail.com>
2021-01-04 20:00:34 +0100

    Zmiana wygladu widgetow

--- lib/pages/counter.dart
+++ lib/pages/counter.dart
@@ -11,18 +11,46 @@ class _CounterPageState extends State<CounterPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.indigoAccent,
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Licznik"),
+        backgroundColor: Colors.redAccent,
+      ),
       body: Center(
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
-            Text(liczba.toString()),
+            Text(
+              liczba.toString(),
+              style: TextStyle(
+                fontSize: 128,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            SizedBox(
+              height: 32,
+            ),
             RaisedButton(
               onPressed: () {
                 setState(() {
                   liczba++;
                 });
               },
-              child: Text("Zwiększ o jeden"),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(100),
+              ),
+              color: Colors.redAccent,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  "Zwiększ o jeden".toUpperCase(),
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
             ),
           ],
         ),
